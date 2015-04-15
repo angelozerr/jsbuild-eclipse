c96b0f088b3b5fec10ff39af1426868ed3117404
angelozerr <angelo.zerr@gmail.com>
2015-04-15 14:12:44 +0200

    Try to fix build.

--- core/fr.opensagres.eclipse.jsbuild.ui/src/fr/opensagres/eclipse/jsbuild/internal/ui/JSBuildFileAdapterFactory.java
+++ core/fr.opensagres.eclipse.jsbuild.ui/src/fr/opensagres/eclipse/jsbuild/internal/ui/JSBuildFileAdapterFactory.java
@@ -2,23 +2,12 @@ package fr.opensagres.eclipse.jsbuild.internal.ui;
 
 import org.eclipse.core.resources.IResource;
 import org.eclipse.core.runtime.IAdapterFactory;
-import org.eclipse.ui.IContainmentAdapter;
-import org.eclipse.ui.IContributorResourceAdapter;
-import org.eclipse.ui.IPersistableElement;
-import org.eclipse.ui.ide.IContributorResourceAdapter2;
-import org.eclipse.ui.model.IWorkbenchAdapter;
-import org.eclipse.ui.views.properties.IPropertySource;
-import org.eclipse.ui.views.tasklist.ITaskListResourceAdapter;
 
 import fr.opensagres.eclipse.jsbuild.core.IJSBuildFileNode;
 
 public class JSBuildFileAdapterFactory implements IAdapterFactory {
 
-	private static Class[] PROPERTIES = new Class[] { IPropertySource.class,
-			IResource.class, IWorkbenchAdapter.class,
-			IPersistableElement.class, IContributorResourceAdapter.class,
-			IContributorResourceAdapter2.class, ITaskListResourceAdapter.class,
-			IContainmentAdapter.class };
+	private static Class[] PROPERTIES = new Class[] { IResource.class };
 
 	@Override
 	public Object getAdapter(Object element, Class key) {
